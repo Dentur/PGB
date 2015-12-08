ab48609bb84e5b24ff9f054ec9a9d5070be10e20
Sebastian Venhuis <sebastian@venhuis.com>
2015-12-08 08:48:24 +0100

    added the usermsg

---
+++ Demo/usermsg.h
@@ -0,0 +1,22 @@
+#ifndef USERMSG_H
+#define USERMSG_H
+
+#include <winuser.h>
+
+#define UPDATE_NAME		WM_USER
+#define UPDATE_REIHE	(WM_USER+1)
+#define UPDATE_WERT		(WM_USER+2)
+#define UPDATE_ALL		(WM_USER+3)
+#define CLOSE_ALL		(WM_USER+4)
+
+#define FLAG_NAME	0x01
+#define FLAG_FARBE	0x02
+#define FLAG_WERT	0x04
+
+extern void update_name();
+extern void update_reihe(int zeile, int name, int farbe);
+extern void update_wert(int zeile, int spalte);
+extern void update_all(int name, int farbe, int  wert);
+extern void close_all();
+
+#endif
